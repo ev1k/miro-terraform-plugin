f0bcba0bf29b8405a707764f1c35dc8de87ce621
Aleksandr Kiryushkin <aleksandr.k@miro.com>
2020-02-14 09:26:41 +0500

    add resources api

--- server/app.py
+++ server/app.py
@@ -1,10 +1,48 @@
 from flask import Flask, request, jsonify
-import hcl2
+from flask_cors import CORS
+
+import json
+from pprint import pprint
+
 
 app = Flask(__name__)
+CORS(app)
+resource_data = None
+reload_resources()
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+    app.logger.debug(request)
+
+@app.route('/get_price', methods=['GET']) 
+def get_price():
+    instanceType = request.values.get('instanceType')
+    region = request.values.get('region')
+    res = find_price(data, instanceType, region)
+    
+    response = app.response_class(
+        response= json.dumps(res)
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/reload_resources', methods=['GET']) 
+def reload_resources():
+    with open('resources.json') as f:
+    data = json.load(f)
+
+def find_price(json_object, instanceType, region):
+    ret = None
+    for node in json_object:
+        if (node['instanceType'] == instanceType and node['region'] == region):
+            ret = node
+    return ret      
 
 @app.route('/parse', methods=['POST']) 
 def parse():
+    print(request)
     content = request.json
     return content
 
