4eac7ea250f4090444f249d5af2aa4cd33dcd7d9
Aleksandr Kiryushkin <aleksandr.k@miro.com>
2020-02-14 10:46:58 +0500

    api improve

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
-
+import copy
 import json
 from pprint import pprint
 
 
 app = Flask(__name__)
 CORS(app)
-resource_data = None
+resource_data = ''
+@app.route('/reload_resources', methods=['GET']) 
+def reload_resources():
+    with open('resources.json') as f:
+        global resource_data
+        resource_data = json.load(f)
 reload_resources()
 
 @app.before_request
@@ -16,29 +21,32 @@ def log_request_info():
     app.logger.debug('Body: %s', request.get_data())
     app.logger.debug(request)
 
-@app.route('/get_price', methods=['GET']) 
-def get_price():
+@app.route('/get_props', methods=['GET']) 
+def get_props():
     instanceType = request.values.get('instanceType')
     region = request.values.get('region')
-    res = find_price(data, instanceType, region)
-    
+    res = find_instance_props(resource_data, instanceType, region)
+   
     response = app.response_class(
-        response= json.dumps(res)
+
+        response= json.dumps(res),
         mimetype='application/json'
     )
     return response
 
-@app.route('/reload_resources', methods=['GET']) 
-def reload_resources():
-    with open('resources.json') as f:
-    data = json.load(f)
 
-def find_price(json_object, instanceType, region):
+
+def find_instance_props(json_object, instanceType, region):
     ret = None
     for node in json_object:
         if (node['instanceType'] == instanceType and node['region'] == region):
             ret = node
-    return ret      
+    if (ret == None):
+        abort(404)
+    copied = copy.deepcopy(ret)
+    del copied['instanceType']
+    del copied['region']
+    return copied      
 
 @app.route('/parse', methods=['POST']) 
 def parse():
@@ -48,8 +56,7 @@ def parse():
 
 @app.route('/parse_test', methods=['POST']) 
 def parse_test():
-    data = '''{
-  "graphs": [
+    data = '''"graphs": [
     {
       "directed": true,
       "type": "graph type",
@@ -58,64 +65,173 @@ def parse_test():
         "user-defined": "values"
       },
       "nodes": {
-        "0": {
-          "type": "node type",
-          "label": "node label(0)",
+        "aws_lb.this[0]": {
+          "type": "aws_lb",
+          "label": "this",
           "metadata": {
-            "user-defined": "values"
+            "name": "this",
+            "lb_type": "application",
+            "display_name": [
+              "web-app"
+            ]
           }
         },
-        "1": {
-          "type": "node type",
-          "label": "node label(1)",
+        "aws_lb_listener.this[0]": {
+          "type": "aws_lb_listener",
+          "label": "this",
           "metadata": {
-            "user-defined": "values"
+            "name": "this",
+            "port": "80",
+            "protocol": "HTTP",
+            "default_action_type": "forward"
+          }
+        },
+        "aws_lb_target_group.this[0]": {
+          "type": "aws_lb_target_group",
+          "label": "this",
+          "metadata": {
+            "name": "this",
+            "vpc_id": "vpc-123",
+            "port": 443,
+            "protocol": "HTTPS",
+            "target_type": "instance"
+          }
+        },
+        "aws_lb_target_group.custom[0]": {
+          "type": "aws_lb_target_group",
+          "label": "custom",
+          "metadata": {
+            "name": "custom",
+            "vpc_id": "vpc-123",
+            "port": 443,
+            "protocol": "HTTPS",
+            "target_type": "instance"
+          }
+        },
+        "aws_instance.this[0]": {
+          "type": "aws_instance",
+          "label": "this",
+          "metadata": {
+            "name": "this",
+            "subnet_id": [
+              "subnet-123"
+            ],
+            "instance_type": "c5.large",
+            "count": 3,
+            "availability_zone": "us-east-1a",
+            "ami": "ami-123"
+          }
+        },
+        "aws_instance.this[1]": {
+          "type": "aws_instance",
+          "label": "this",
+          "metadata": {
+            "name": "this",
+            "subnet_id": [
+              "subnet-123"
+            ],
+            "instance_type": "c5.large",
+            "count": 3,
+            "availability_zone": "us-east-1a",
+            "ami": "ami-123"
+          }
+        },
+        "aws_instance.this[2]": {
+          "type": "aws_instance",
+          "label": "this",
+          "metadata": {
+            "name": "this",
+            "subnet_id": [
+              "subnet-123"
+            ],
+            "instance_type": "c5.large",
+            "count": 3,
+            "availability_zone": "us-east-1a",
+            "ami": "ami-123"
+          }
+        },
+        "aws_instance.custom[0]": {
+          "type": "aws_instance",
+          "label": "custom",
+          "metadata": {
+            "name": "custom",
+            "subnet_id": [
+              "subnet-456"
+            ],
+            "instance_type": "m5.large",
+            "count": 1,
+            "availability_zone": "us-east-1a",
+            "ami": "ami-456"
           }
         }
       },
       "edges": [
         {
-          "source": "0",
+          "source": "aws_lb.this[0]",
           "relation": "edge relationship",
-          "target": "1",
+          "target": "aws_lb_listener.this[0]",
           "directed": true,
-          "label": "edge label",
-          "metadata": {
-            "user-defined": "values"
-          }
-        }
-      ]
-    },
-    {
-      "directed": true,
-      "type": "graph type",
-      "label": "graph label",
-      "metadata": {
-        "user-defined": "values"
-      },
-      "nodes": {
-        "0": {
-          "type": "node type",
-          "label": "node label(0)",
+          "label": "",
           "metadata": {
             "user-defined": "values"
           }
         },
-        "1": {
-          "type": "node type",
-          "label": "node label(1)",
+        {
+          "source": "aws_lb_listener.this[0]",
+          "relation": "edge relationship",
+          "target": "aws_lb_target_group.this[0]",
+          "directed": true,
+          "label": "",
           "metadata": {
             "user-defined": "values"
           }
-        }
-      },
-      "edges": [
+        },
         {
-          "source": "1",
+          "source": "aws_lb_listener.this[0]",
           "relation": "edge relationship",
-          "target": "0",
+          "target": "aws_lb_target_group.custom[0]",
           "directed": true,
-          "label": "edge label",
+          "label": "",
+          "metadata": {
+            "user-defined": "values"
+          }
+        },
+        {
+          "source": "aws_lb_target_group.this[0]",
+          "relation": "edge relationship",
+          "target": "aws_instance.this[0]",
+          "directed": true,
+          "label": "",
+          "metadata": {
+            "user-defined": "values"
+          }
+        },
+        {
+          "source": "aws_lb_target_group.this[0]",
+          "relation": "edge relationship",
+          "target": "aws_instance.this[1]",
+          "directed": true,
+          "label": "",
+          "metadata": {
+            "user-defined": "values"
+          }
+        },
+        {
+          "source": "aws_lb_target_group.this[0]",
+          "relation": "edge relationship",
+          "target": "aws_instance.this[2]",
+          "directed": true,
+          "label": "",
+          "metadata": {
+            "user-defined": "values"
+          }
+        },
+        {
+          "source": "aws_lb_target_group.custom[0]",
+          "relation": "edge relationship",
+          "target": "aws_instance.custom[0]",
+          "directed": true,
+          "label": "",
           "metadata": {
             "user-defined": "values"
           }
